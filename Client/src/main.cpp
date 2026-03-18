f1f3e68af91022e2ddb24968ad617fd769c72550
canberk pıtırlı <canberk_nurcan@hotmail.com>
2026-03-18 14:41:13 +0300

    Set working directory to executable path on startup

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -2,7 +2,12 @@
 #include <Core/Application.hpp>
 #include <Game.hpp>
 #include <string>
+#include <filesystem>
 #include <Logger.hpp>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 #define PROJECT_NAME "SleakEngine"
 
@@ -14,7 +19,23 @@ std::string HelpMessage =
   -t :    Sets title of the window, to set space between words put _ character \n\
   ";
 
+static void SetWorkingDirToExePath(char* argv0) {
+#ifdef _WIN32
+    char path[MAX_PATH];
+    if (GetModuleFileNameA(nullptr, path, MAX_PATH) != 0) {
+        std::filesystem::current_path(
+            std::filesystem::path(path).parent_path());
+        return;
+    }
+#endif
+    auto exeDir = std::filesystem::path(argv0).parent_path();
+    if (!exeDir.empty())
+        std::filesystem::current_path(exeDir);
+}
+
 int main(int argc, char** argv) {
+
+  SetWorkingDirToExePath(argv[0]);
 
   Sleak::Logger::Init((char*)PROJECT_NAME);
 
